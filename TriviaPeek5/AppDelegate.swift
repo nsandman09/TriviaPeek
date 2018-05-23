bea9834948547cae0460315a4c4adcfd69d54fb6
Noah Sandman <nsandman09@gmail.com>
2018-05-22 22:33:21 -0700

    Initial Commit

---
+++ TriviaPeek5/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TriviaPeek5
+//
+//  Created by Noah Sandman on 5/22/18.
+//  Copyright © 2018 Noah Sandman. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TriviaPeek5/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TriviaPeek5
+//
+//  Created by Noah Sandman on 5/22/18.
+//  Copyright © 2018 Noah Sandman. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
