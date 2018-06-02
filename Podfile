e0389beb906012163ca705773ddc05dce572cad1
Noah Sandman <nsandman09@gmail.com>
2018-06-01 21:01:47 -0700

    initial commit

---
+++ Podfile
@@ -0,0 +1,14 @@
+project 'TriviaPeek5.xcodeproj'
+
+source 'https://github.com/nsandman09/YWebView'
+
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'TriviaPeek5' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for TriviaPeek5
+  pod "YWebView"
+end
--- TriviaPeek5/ViewController.swift
+++ TriviaPeek5/ViewController.swift
@@ -7,13 +7,42 @@
 //
 
 import Cocoa
+import WebKit
 
 class ViewController: NSViewController {
-
+    @IBOutlet weak var window: NSWindow!
+    
+    @IBOutlet weak var googleWebView: WKWebView!
+    @IBOutlet weak var discordWebView: WKWebView!
+    @IBOutlet weak var triviaQuackWebView: WKWebView!
+//  @IBOutlet weak var hqtLiveWebView: WKWebView!
+//  @IBOutlet weak var hqtLegacyWebView: WebView!
+    @IBOutlet weak var triviaPartyLegacyView: WebView!
+    @IBOutlet weak var triviaCrowdWebView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let googleUrl = URL(string: "https://www.google.com")
+        let googleUrlReq = URLRequest(url: googleUrl!)
+        googleWebView.customUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 10_0_1 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Mobile/14A403 Safari/602.1"
+        googleWebView.load(googleUrlReq)
+        
+        let discordUrl = URL(string: "https://discordapp.com/channels/382761915811627009/444975842825732096")
+        let discordUrlReq = URLRequest(url: discordUrl!)
+        discordWebView.load(discordUrlReq)
+        
+        let triviaQuackUrl = URL(string: "https://triviaquack.us")
+        let triviaQuackUrlReq = URLRequest(url: triviaQuackUrl!)
+        triviaQuackWebView.load(triviaQuackUrlReq)
+     
+//      hqtLegacyWebView.mainFrameURL = "https://live.hqt.space";
+        triviaPartyLegacyView.mainFrameURL = "https://live.trivia.party/";
+        
+        let triviaCrowdUrl = URL(string: "https://triviacrowd.live")
+        let triviaCrowdUrlReq = URLRequest(url: triviaCrowdUrl!)
+        triviaCrowdWebView.load(triviaCrowdUrlReq)
     }
 
     override var representedObject: Any? {
